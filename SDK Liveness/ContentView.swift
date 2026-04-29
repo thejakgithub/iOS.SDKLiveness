d982baa6095d408ff744567b82d555074884b8e4
Switt Chaisri <switt_cha@freewillsolutions.com>
2026-04-29 17:01:59 +0700

    Initial Commit

---
+++ SDK Liveness/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SDK Liveness
+//
+//  Created by mdc on 29/4/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SDK Liveness/SDK_LivenessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDK_LivenessApp.swift
+//  SDK Liveness
+//
+//  Created by mdc on 29/4/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct SDK_LivenessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
